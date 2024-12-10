37dca651e43e10a00ba55f55d16317cb2e30621e
hayksmn <hayksmn@gmail.com>
2024-12-10 23:31:13 +0400

    moved dotenv config to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata'
 import express from 'express'
 import loaders from './libraries/loaders'
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const startApp = async () => {
     const app = express()
